c68b3a4fcd1cf9c3f73ac8c3a2bc6b0a8e84430d
M245121 <alexandre.vogelsanger@everis.com>
2023-05-09 21:36:40 -0300

    Project created

---
+++ src/test/java/com/devsuperior/dslists/DslistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
